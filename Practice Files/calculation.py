aca58d4c95f1700b3c811ff8ce8679d59f055587
Niraj Khankari <156000340+NirajKhankari30@users.noreply.github.com>
2025-10-03 02:49:16 +0530

    Add files via upload

---
+++ Practice Files/calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#In[ ]:
+
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+def modulus(a, b):
+    return a % b
+
+def floordivision(a, b):
+    return a // b
